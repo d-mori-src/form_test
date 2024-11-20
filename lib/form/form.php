f0dd819733ba09544e3bfa3c8dc9706e231dc269
d-mori-src <d-mori@src-japan.net>
2024-11-20 14:30:12 +0900

    first commit

---
+++ lib/form/form.php
@@ -0,0 +1,54 @@
+<?php
+    date_default_timezone_set('Asia/Tokyo');
+    mb_language('uni');
+    mb_internal_encoding('UTF-8');
+
+    $esc = [];
+    $flag = 0;
+    $error = [];
+
+    require_once 'inc/validation.php';
+    require_once 'inc/escape.php';
+
+    // 状況に応じてフラグの切り替え
+    if (!empty($esc['confirm'])) {
+        // 「確認画面へ」ボタンが押された時の処理
+        $error = validation($esc);
+        if(empty($error)) {
+            $flag = 1;
+            $_SESSION['page'] = true;
+
+            $upload = './strage/'.$_FILES['file_upload']['name']; 
+            //アップロードが正しく完了したかチェック
+            if(move_uploaded_file($_FILES['file_upload']['tmp_name'], $upload)){
+                echo 'アップロード完了';
+            }else{
+                echo 'アップロード失敗'; 
+            }
+        }
+    } elseif (!empty($esc['submit'])) {
+        session_start();
+
+        if (!empty($_SESSION['page']) && $_SESSION['page'] === true) {
+            // 「送信」ボタンが押された時の処理
+            $flag = 2;
+            require_once 'inc/mail.php';
+            unset($_SESSION['page']);
+        } else {
+            $flag = 0;
+        }
+    } else {
+        $flag = 0;
+    }
+
+    // ビューの描画
+    if ($flag === 1) {
+        // 確認画面のHTMLコード
+        require_once 'template/confirm.php';
+    } elseif($flag === 2) {
+        // 送信完了画面のHTMLコード 
+        require_once 'template/thanks.php';
+    } else {
+        // お問い合わせフォームのHTMLコード
+        require_once 'template/contact.php';
+    }
